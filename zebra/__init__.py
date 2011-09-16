61746c47d6a6c703d86252d5f161167f41246822
Steven Skoczen <steven@quantumimagery.com>
2011-09-15 22:04:25 -0700

    a little more abstraction and nicer printing

--- zebra/__init__.py
+++ zebra/__init__.py
@@ -1,3 +1,5 @@
 import datetime
 REASONABLE_YEARS = range(datetime.date.today().year, datetime.date.today().year+40)
 REASONABLE_YEAR_CHOICES = [(i,i) for i in REASONABLE_YEARS]
+
+MAXIMUM_STRIPE_CUSTOMER_LIST_SIZE = 100
--- zebra/management/commands/clear_stripe_test_customers.py
+++ zebra/management/commands/clear_stripe_test_customers.py
@@ -1,17 +1,41 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
-
+from zebra import MAXIMUM_STRIPE_CUSTOMER_LIST_SIZE
 import stripe
+
+CLEAR_CHUNK_SIZE = MAXIMUM_STRIPE_CUSTOMER_LIST_SIZE
+
 
 class Command(BaseCommand):
     help = "Clear all test mode customers from your stripe account."
     __test__ = False
 
     def handle(self, *args, **options):
+        verbosity = int(options.get('verbosity', 1))
         stripe.api_key = settings.STRIPE_SECRET
-        print "Clearing stripe test customers..."
-        for c in stripe.Customer.all():
-            print "Deleting %s..." % (c.description),
-            if not c.livemode:
-                c.delete()
-                print "done"
+        customer_chunk = [0]
+
+        if verbosity > 0:
+            print "Clearing stripe test customers:"
+        
+        num_checked = 0
+        while customer_chunk is not []:
+            customer_chunk = stripe.Customer.all(count=CLEAR_CHUNK_SIZE).data
+
+            if verbosity > 1:     
+                print "Processing records %s-%s" % (num_checked, num_checked+len(customer_chunk))
+            
+            for c in customer_chunk:
+                if verbosity > 2:
+                    print "Deleting %s..." % (c.description),
+
+                if not c.livemode:
+                    c.delete()
+
+                    if verbosity > 2:
+                        print "done"
+            
+            num_checked = num_checked + len(customer_chunk)
+        
+        if verbosity > 0:
+            print "Finished clearing stripe test customers."
