fffe50046d1438e34a8044d294961454f88268e1
Lee Trout <lee@leetrout.com>
2011-09-17 21:32:43 -0400

    updated audit result defaults. added active / inactive status tuples

--- zebra/conf/defaults.py
+++ zebra/conf/defaults.py
@@ -16,17 +16,22 @@ except KeyError:
     STRIPE_SECRET = ''
 
 ZEBRA_ENABLE_APP = False
+
 ZEBRA_TODAY = datetime.date.today()
-ZEBRA_CARD_YEARS = range(ZEBRA_TODAY.year, ZEBRA_TODAY.year+10)
+ZEBRA_CARD_YEARS = range(ZEBRA_TODAY.year, ZEBRA_TODAY.year+12)
 ZEBRA_CARD_YEARS_CHOICES = [(i,i) for i in ZEBRA_CARD_YEARS]
 
 ZEBRA_MAXIMUM_STRIPE_CUSTOMER_LIST_SIZE = 100
 
 ZEBRA_AUDIT_RESULTS = {
-    'active': (True, 'active'),
-    'no_subscription': (False, 'no_subscription'),
-    'past_due': (True, 'past_due'),
-    'suspended': (False, 'suspended'),
-    'trialing': (True, 'trialing'),
-    'unpaid': (False, 'cancelled')
+    'active': 'active',
+    'no_subscription': 'no_subscription',
+    'past_due': 'past_due',
+    'suspended': 'suspended',
+    'trialing': 'trialing',
+    'unpaid': 'unpaid',
+    'cancelled': 'cancelled'
 }
+
+ZEBRA_ACTIVE_STATUSES = ('active', 'past_due', 'trialing')
+ZEBRA_INACTIVE_STATUSES = ('cancelled', 'suspended', 'unpaid', 'no_subscription')
