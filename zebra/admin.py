9f1d193f68a5649d95d5e83888d5f182fa552892
Steven Skoczen <steven@quantumimagery.com>
2011-09-18 13:48:14 -0700

    conditional import on ZEBRA_ENABLE_APP for admin

--- zebra/admin.py
+++ zebra/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 from zebra.conf import settings
-from zebra.models import Customer, Plan, Subscription
 
 if settings.ZEBRA_ENABLE_APP:
+
+    from zebra.models import Customer, Plan, Subscription
     admin.site.register(Customer)
     admin.site.register(Plan)
     admin.site.register(Subscription)
