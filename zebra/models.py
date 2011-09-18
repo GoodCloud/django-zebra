22fde014a82ffafa1e3e34d7f20cc49685fa35a2
Lee Trout <lee@leetrout.com>
2011-09-17 23:45:57 -0400

    tweaked models- moved __unicode__ to ABC and date ABC behind enable_app switch

--- zebra/models.py
+++ zebra/models.py
@@ -4,19 +4,14 @@ from zebra import mixins
 from zebra.conf import settings
 
 
-class DatesModelBase(models.Model):
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_modified = models.DateTimeField(auto_now=True)
-    
-    class Meta:
-        abstract = True
-
-
 class StripeCustomer(models.Model, mixins.StripeMixin, mixins.StripeCustomerMixin):
     stripe_customer_id = models.CharField(max_length=50)
     
     class Meta:
         abstract = True
+    
+    def __unicode__(self):
+            return self.stripe_customer_id
 
 
 class StripePlan(models.Model, mixins.StripeMixin, mixins.StripePlanMixin):
@@ -24,6 +19,9 @@ class StripePlan(models.Model, mixins.StripeMixin, mixins.StripePlanMixin):
     
     class Meta:
         abstract = True
+    
+    def __unicode__(self):
+            return self.stripe_plan_id
 
 
 class StripeSubscription(models.Model, mixins.StripeMixin, mixins.StripeSubscriptionMixin):
@@ -37,13 +35,18 @@ class StripeSubscription(models.Model, mixins.StripeMixin, mixins.StripeSubscrip
 
 # Non-abstract classes must be enabled in your project's settings.py
 if settings.ZEBRA_ENABLE_APP:
+    class DatesModelBase(models.Model):
+        date_created = models.DateTimeField(auto_now_add=True)
+        date_modified = models.DateTimeField(auto_now=True)
+        
+        class Meta:
+            abstract = True
+    
     class Customer(DatesModelBase, StripeCustomer):
-        def __unicode__(self):
-            return self.stripe_customer_id
+        pass
     
     class Plan(DatesModelBase, StripePlan):
-        def __unicode__(self):
-            return self.stripe_plan_id
+        pass
     
     class Subscription(DatesModelBase, StripeSubscription):
         customer = models.ForeignKey(Customer)
