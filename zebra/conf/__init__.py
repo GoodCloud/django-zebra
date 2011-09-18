20f353dc793701e729860ead51bd7b27dbb11e38
Lee Trout <lee@leetrout.com>
2011-09-17 22:41:01 -0400

    fixed error caused by improper settings handling

--- zebra/conf/__init__.py
+++ zebra/conf/__init__.py
@@ -1,6 +1,10 @@
 from zebra.conf import defaults
-from django.conf import settings
+from django.conf import settings as _settings
+from django.conf import UserSettingsHolder
 
+ush = UserSettingsHolder(_settings)
 for attr in dir(defaults):
-    if not hasattr(settings, attr):
-        setattr(settings, attr, getattr(defaults, attr))
+    if attr.upper().startswith('ZEBRA') or attr.upper().startswith('STRIPE'):
+        if not hasattr(ush, attr):
+            setattr(ush, attr, getattr(defaults, attr))
+settings = ush
