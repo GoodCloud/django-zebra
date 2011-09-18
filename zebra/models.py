94501e454ab5a0c7abc38af700f8341e13a6f072
Lee Trout <lee@leetrout.com>
2011-09-17 23:38:37 -0400

    cleaned up models and fixed missing plan model in admin

--- zebra/models.py
+++ zebra/models.py
@@ -12,16 +12,25 @@ class DatesModelBase(models.Model):
         abstract = True
 
 
-class StripeCustomer(models.Model, mixins.StripeCustomerMixin):
+class StripeCustomer(models.Model, mixins.StripeMixin, mixins.StripeCustomerMixin):
     stripe_customer_id = models.CharField(max_length=50)
     
     class Meta:
         abstract = True
 
 
-class StripePlan(models.Model, mixins.StripePlanMixin):
+class StripePlan(models.Model, mixins.StripeMixin, mixins.StripePlanMixin):
     stripe_plan_id = models.CharField(max_length=50)
     
+    class Meta:
+        abstract = True
+
+
+class StripeSubscription(models.Model, mixins.StripeMixin, mixins.StripeSubscriptionMixin):
+    """
+    You need to provide a stripe_customer attribute. See zebra.models for an
+    example implimentation.
+    """
     class Meta:
         abstract = True
     
@@ -29,14 +38,19 @@ class StripePlan(models.Model, mixins.StripePlanMixin):
 # Non-abstract classes must be enabled in your project's settings.py
 if settings.ZEBRA_ENABLE_APP:
     class Customer(DatesModelBase, StripeCustomer):
-        pass
+        def __unicode__(self):
+            return self.stripe_customer_id
     
     class Plan(DatesModelBase, StripePlan):
-        pass
+        def __unicode__(self):
+            return self.stripe_plan_id
     
-    class Subscription(DatesModelBase, mixins.StripeSubscriptionMixin):
+    class Subscription(DatesModelBase, StripeSubscription):
         customer = models.ForeignKey(Customer)
         plan = models.ForeignKey(Plan)
+        
+        def __unicode__(self):
+            return "%s: %s" % (self.customer, self.plan)
         
         @property
         def stripe_customer(self):
