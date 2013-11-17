1a844a2aedc400eab467285a026cd4f60cc9a0fd
Steven Skoczen <steven@quantumimagery.com>
2013-11-17 15:15:13 -0800

    Version number bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author = "Steven Skoczen",
     author_email = "steven@agoodcloud.com",
     url = "https://github.com/GoodCloud/django-zebra",
-    version = "0.4.3",
+    version = "0.4.4",
     packages = find_packages(),
     zip_safe = False,
     include_package_data=True,
