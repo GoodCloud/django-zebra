fdb2f2eb9eddb83eebb339359e629c88f4f6ba17
Jared Morse <jarcoal@gmail.com>
2014-06-23 15:08:32 -0700

    bump version to 0.4.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author = "Steven Skoczen",
     author_email = "steven@agoodcloud.com",
     url = "https://github.com/GoodCloud/django-zebra",
-    version = "0.4.4",
+    version = "0.4.5",
     packages = find_packages(),
     zip_safe = False,
     include_package_data=True,
